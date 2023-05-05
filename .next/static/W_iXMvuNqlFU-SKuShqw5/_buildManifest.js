040d9718b6a59b70bd44149128b3349adfd2c3f4
jeevitha-jr <jeevitha.jr@exathought.com>
2023-05-05 11:21:25 +0530

    new relic commit

--- .next/static/W_iXMvuNqlFU-SKuShqw5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-47984490dc1a9486.js","static/chunks/pages/index-34f6ef839156689c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,54 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript,DocumentContext } from "next/document";
+// import newrelic from "newrelic";
+const newrelic= require("newrelic")
+import { logger } from "../components/Logger";
+
 
 class MyDocument extends Document {
+  // static async getInitialProps(
+  //   ctx
+  // ) {
+    // const initialProps = await Document.getInitialProps(ctx);
+
+    // /**
+    //  * For SSG pages the build is faster than the agent connect cycle
+    //  * In those cases, let's wait for the agent to connect before getting
+    //  * the browser agent script.
+    //  */
+    // // if (!newrelic.agent.collector.isConnected()) {
+    // //   // console.log("FIRST",newrelic)
+    // //   await new Promise((resolve) => {
+    // //     console.log("INSIDE")
+    // //     newrelic.setUserID("user-1234-id");
+    // //     newrelic.setTransactionName("test-transaction-name")
+    // //     // newrelic.agent.on("connected", resolve);
+    // //   });
+    // // }
+
+    // const browserTimingHeader =  newrelic.getBrowserTimingHeader({
+    //   hasToRemoveScriptWrapper: true,
+    //   allowTransactionlessInjection: true,
+    // });
+    // console.log("AFTER",newrelic.agent)
+    // logger.info("NextJs New Relic redirecting to a page", {
+    //   application: "NextJs NewRelic app logging",
+    //   test: "Testing logging",
+    //   pathname: "test path",
+    // });
+    // newrelic.setUserID("user-1234-id");
+    // newrelic.setTransactionName("test-transaction-name")
+    // newrelic.recordCustomEvent({eventType: "test-event-123", attributes: {"test":"1234"}});
+    // return {
+    //   ...initialProps,
+    //   // browserTimingHeader,
+    // };
+  // const data=newrelic.setUserID("newrelicUserId");
+  // console.log("DATA",data)
+  //   return{
+  //     ...initialProps,
+  //     data
+  //   }
+  // }
   render() {
     return (
       <Html>
