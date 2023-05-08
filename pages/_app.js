b569c42bdcd08b5e7db53abff01799f95de858a3
suhas-predictive <suhas.bharadwaj@predictive.fit>
2023-05-08 11:26:27 +0530

    commit with code for 2 measurement ids

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,50 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 // import { pageview } from "../lib/ga";
+import { usePathname, useSearchParams } from 'next/navigation';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+  let projectId = "G-H382LDYLBZ";
+  const searchParams = useSearchParams();
+  let fromMobile = searchParams.get('fromMobile');
+  
+  if(fromMobile == 1){
+    projectId = "G-N32L6MLLWS";
+  }
 
-  // useEffect(() => {
-  //   const handleRouteChange = (url) => {
-  //     pageview(url);
-  //   };
-
-  //   //When the component is mounted, subscribe to router changes
-  //   //and log those page views
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-
-  //   // If the component is unmounted, unsubscribe
-  //   // from the event with the `off` method
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
-
-  return <div>Hello World!</div>;
+  console.log("project id --", projectId);
+  
+return (
+  
+  <>
+  <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${projectId}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${projectId}', {
+              page_path: window.location.pathname,
+          });
+          window.gtag('event', 'click_button', {
+            'event_category': 'Button',
+            'event_label': 'My Button'
+          });
+          `,
+          }}
+        />
+    <Link href="/page2">Go to page 2 _app.js</Link>
+    <button>
+      Click me
+    </button>
+  </>
+);
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,33 @@ import Link from "next/link";
 
 export default function Home() {
   return (
+    
     <>
-      <Link href="/page2">Go to page 2</Link>
+    {/* <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${projectId}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${projectId}, {
+                page_path: window.location.pathname,
+            });
+            window.gtag('event', 'click_button', {
+              'event_category': 'Button',
+              'event_label': 'My Button'
+            });
+            `,
+            }}
+          />
+      <Link href="/page2">Go to page 2 indexjs</Link>
       <button
       >
         Click me
-      </button>
+      </button> */}
     </>
   );
 }
